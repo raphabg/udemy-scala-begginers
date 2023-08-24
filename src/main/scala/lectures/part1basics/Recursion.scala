bf472b3bb2b8a409af01dd8a4a1b867e0acecfcc
Raphael Balogo <balogoraphael@gmail.com>
2023-08-23 22:31:17 -0300

    Resolution of Recursion exercises, before lesson 6 end

---
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -0,0 +1,77 @@
+package lectures.part1basics
+
+import scala.annotation.tailrec
+import scala.jdk.Accumulator
+
+object Recursion extends App {
+  def factorial(n: Int): Int = {
+    if (n <= 1) 1
+    else {
+      println(
+        "Computing factorial of " + n + " - I first need factorial of " + (n - 1)
+      )
+      var result = n * factorial(n - 1)
+      println(s"Computed factorial of $n")
+
+      result
+    }
+  }
+
+  //factorial(5000)
+
+  def anotherFactorial(n: Int): BigInt = {
+    def factorialHelper(x: Int, accumulator: BigInt): BigInt = {
+      if (x <= 1) accumulator
+      else factorialHelper(x - 1, x * accumulator) // TAIL RECURSION
+    }
+
+    factorialHelper(n, 1)
+  }
+
+  println(anotherFactorial(5))
+  // WHEN YOU NEED LOOPS, USE _TAIL_ RECURSION.
+
+  /*
+   * 1. Concatenate a string n times
+   * 2. IsPrime function tail recursive
+   * 3. Fibonacci function, tail recursive
+   * */
+
+  @tailrec
+  def concatenateRecursive(
+      string: String,
+      n: Int,
+      accumulator: String = ""
+  ): String = {
+    val result = accumulator + string
+
+    if (n == 1) result
+    else concatenateRecursive(string, n - 1, result)
+  }
+
+  println(concatenateRecursive("a|", 7))
+
+  def isPrimeRecursive() = {}
+
+  @tailrec
+  def fibonacciRecursive(
+      desiredPos: Int,
+      current: Int = 0,
+      previous: Int = 0,
+      recursionLevel: Int = 0
+  ): Int = {
+    if (recursionLevel < desiredPos) {
+      if (recursionLevel == 0)
+        fibonacciRecursive(desiredPos, 1, 0, 1)
+      else
+        fibonacciRecursive(
+          desiredPos,
+          current + previous,
+          current,
+          recursionLevel + 1
+        )
+    } else current;
+  }
+
+  println(fibonacciRecursive(20))
+}
